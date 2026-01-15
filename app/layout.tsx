7ad7d54633afa0e8b79668f9a85e9f23bf4ead87
HarveySpecter85 <harveyrodelousa@gmail.com>
2026-01-15 01:21:43 -0500

    feat: add Vercel Analytics to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
@@ -72,6 +73,7 @@ export default function RootLayout({
         <SmoothScroller>
           <SiteHeader />
           {children}
+          <Analytics />
           <SiteFooter />
         </SmoothScroller>
       </body>
